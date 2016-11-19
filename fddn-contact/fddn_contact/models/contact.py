95e45870c1088bfba0163ce44f73e72515617260
Sherwood Wang <sherwood@wang.onl>
2016-11-19 12:04:50 +0800

    add code

--- fddn-contact/fddn_contact/models/contact.py
+++ fddn-contact/fddn_contact/models/contact.py
@@ -2,6 +2,7 @@ from sqlalchemy import (
     Column,
     Index,
     Integer,
+    Unicode,
     Text,
     ForeignKey,
 )
@@ -13,12 +14,16 @@ class Contact(Base):
     __tablename__ = 'contact'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    name = Column(Text)
+    name = Column(Unicode(50))
 
 
 class ContactInfoItem(Base):
     __tablename__ = 'contact_info_item'
     id = Column(Integer, primary_key=True)
     contact_id = Column(Integer, ForeignKey('contact.id'), nullable=False)
-    key = Column(Integer)
+    key = Column(Unicode(20))
     value = Column(Text)
+
+
+Index('contact_info_item_contact_id_idx', ContactInfoItem.contact_id)
+Index('contact_user_id_idx', ContactInfoItem.contact_id)
