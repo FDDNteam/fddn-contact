2d84189958108cfe1ad0d7b8fae94d65ca87e704
Sherwood Wang <sherwood@wang.onl>
2016-12-03 10:20:45 +0800

    add code

--- fddn-contact/fddn_contact/views/contact_update.py
+++ fddn-contact/fddn_contact/views/contact_update.py
@@ -5,8 +5,7 @@ from ..models import Contact, ContactInfoItem
 
 @view_config(route_name='home', renderer='json')
 def contact_update_view(request):
-    query = request.dbsession.query(Contact)
     one = request.json_body
-    query(ContactInfoItem).filter(ContactInfoItem.contact_id == one['id']).delete()
-    query(Contact).filter(Contact.id == one['id']).update({Contact.name: one['name']})
+    request.dbsession.query(ContactInfoItem).filter(ContactInfoItem.contact_id == one['id']).delete()
+    request.dbsession.query(Contact).filter(Contact.id == one['id']).update({Contact.name: one['name']})
     return {"result":"OK"}
