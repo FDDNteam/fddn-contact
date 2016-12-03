682f5a5697c79bcda285407810cd5815f6a2e195
Sherwood Wang <sherwood@wang.onl>
2016-12-03 10:16:58 +0800

    add code

--- fddn-contact/fddn_contact/views/contact_update.py
+++ fddn-contact/fddn_contact/views/contact_update.py
@@ -1,14 +1,11 @@
-from pyramid.response import Response
 from pyramid.view import view_config
 
-from sqlalchemy.exc import DBAPIError
-
-from ..models import contact
+from ..models import Contact, ContactInfoItem
 
 
 @view_config(route_name='home', renderer='json')
 def contact_update_view(request):
-    query = request.dbsession.query(contact)
+    query = request.dbsession.query(Contact)
     one = request.json_body
     query(ContactInfoItem).filter(ContactInfoItem.contact_id == one['id']).delete()
     query(Contact).filter(Contact.id == one['id']).update({Contact.name: one['name']})
