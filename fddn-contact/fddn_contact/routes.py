a93864d59ee7f3aef57ba73eb2293b32a1631a7b
Sherwood Wang <sherwood@wang.onl>
2016-12-10 12:02:20 +0800

    improve

--- fddn-contact/fddn_contact/routes.py
+++ fddn-contact/fddn_contact/routes.py
@@ -1,5 +1,4 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    config.add_route('set-contact','/set_contact')
-    config.add_route('get-contact','/get_contact')
+    config.add_route('contact','/contact')
--- fddn-contact/fddn_contact/views/contact_update.py
+++ fddn-contact/fddn_contact/views/contact_update.py
@@ -3,68 +3,68 @@ from pyramid.view import view_config
 from ..models import Contact, ContactInfoItem
 
 
-@view_config(route_name='set-contact', renderer='json')
+@view_config(route_name='contact', renderer='json')
 def contact_set(request):
-    contact_data = request.json_body
+    if request.method == 'POST':
+        contact_data = request.json_body
 
-    if 'id' in contact_data:
+        if 'id' in contact_data:
+            contact = request.dbsession \
+                .query(Contact) \
+                .filter(Contact.id == contact_data['id']) \
+                .first()
+
+            if contact is None:
+                return {'result': 'failed', 'reason': 'object not found'}
+
+            request.dbsession\
+                .query(ContactInfoItem)\
+                .filter(ContactInfoItem.contact_id == contact_data['id'])\
+                .delete()
+
+            request.dbsession \
+                .query(Contact) \
+                .filter(Contact.id == contact_data['id']) \
+                .update({Contact.name: contact_data['name']})
+        else:
+            contact = Contact()
+            contact.name = contact_data['name']
+            request.dbsession.add(contact)
+            request.dbsession.flush()
+            request.dbsession.refresh(contact)
+
+        for i, item_data in enumerate(contact_data['items']):
+            item = ContactInfoItem()
+            item.contact_id = contact_data['id']
+            item.key = item_data['key']
+            item.value = item_data['value']
+            item.index = i
+            request.dbsession.add(item)
+
+        return {'result': 'ok', 'id': contact.id}
+    elif request.method == 'GET':
+        contact_id = request.params['id']
         contact = request.dbsession \
             .query(Contact) \
-            .filter(Contact.id == contact_data['id']) \
+            .filter(Contact.id == contact_id) \
             .first()
 
         if contact is None:
             return {'result': 'failed', 'reason': 'object not found'}
 
-        request.dbsession\
-            .query(ContactInfoItem)\
-            .filter(ContactInfoItem.contact_id == contact_data['id'])\
-            .delete()
+        items = request.dbsession \
+            .query(ContactInfoItem) \
+            .filter(ContactInfoItem.contact_id == contact_id) \
+            .all()
 
-        request.dbsession \
-            .query(Contact) \
-            .filter(Contact.id == contact_data['id']) \
-            .update({Contact.name: contact_data['name']})
+        return {
+            'id': contact.id,
+            'name': contact.name,
+            'items': [{
+                          'key': item.key,
+                          'value': item.value,
+                      } for item in items]
+        }
     else:
-        contact = Contact()
-        contact.name = contact_data['name']
-        request.dbsession.add(contact)
-        request.dbsession.flush()
-        request.dbsession.refresh(contact)
+        return {'result': 'failed'}
 
-    for i, item_data in enumerate(contact_data['items']):
-        item = ContactInfoItem()
-        item.contact_id = contact_data['id']
-        item.key = item_data['key']
-        item.value = item_data['value']
-        item.index = i
-        request.dbsession.add(item)
-
-    return {'result': 'ok', 'id': contact.id}
-
-
-@view_config(route_name='get-contact', renderer='json')
-def contact_get(request):
-    contact_data = request.json_body
-
-    contact = request.dbsession \
-        .query(Contact) \
-        .filter(Contact.id == contact_data['id']) \
-        .first()
-
-    if contact is None:
-        return {'result': 'failed', 'reason': 'object not found'}
-
-    items = request.dbsession \
-        .query(ContactInfoItem) \
-        .filter(ContactInfoItem.contact_id == contact_data['id']) \
-        .all()
-
-    return {
-        'id': contact.id,
-        'name': contact.name,
-        'items': [{
-            'key': item.key,
-            'value': item.value,
-        } for item in items]
-    }
