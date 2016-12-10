43c0f95649a6f498ea33886dca842257a7e792b8
Sherwood Wang <sherwood@wang.onl>
2016-12-10 11:48:49 +0800

    improve

--- fddn-contact/fddn_contact/routes.py
+++ fddn-contact/fddn_contact/routes.py
@@ -1,4 +1,5 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    config.add_route('update','/update')
+    config.add_route('set-contact','/set_contact')
+    config.add_route('get-contact','/get_contact')
--- fddn-contact/fddn_contact/views/contact_update.py
+++ fddn-contact/fddn_contact/views/contact_update.py
@@ -3,8 +3,8 @@ from pyramid.view import view_config
 from ..models import Contact, ContactInfoItem
 
 
-@view_config(route_name='update', renderer='json')
-def contact_update_view(request):
+@view_config(route_name='set-contact', renderer='json')
+def contact_set(request):
     contact_data = request.json_body
 
     if 'id' in contact_data:
@@ -29,6 +29,8 @@ def contact_update_view(request):
         contact = Contact()
         contact.name = contact_data['name']
         request.dbsession.add(contact)
+        request.dbsession.flush()
+        request.dbsession.refresh(contact)
 
     for i, item_data in enumerate(contact_data['items']):
         item = ContactInfoItem()
@@ -38,4 +40,31 @@ def contact_update_view(request):
         item.index = i
         request.dbsession.add(item)
 
-    return {"result": "ok"}
+    return {'result': 'ok', 'id': contact.id}
+
+
+@view_config(route_name='get-contact', renderer='json')
+def contact_get(request):
+    contact_data = request.json_body
+
+    contact = request.dbsession \
+        .query(Contact) \
+        .filter(Contact.id == contact_data['id']) \
+        .first()
+
+    if contact is None:
+        return {'result': 'failed', 'reason': 'object not found'}
+
+    items = request.dbsession \
+        .query(ContactInfoItem) \
+        .filter(ContactInfoItem.contact_id == contact_data['id']) \
+        .all()
+
+    return {
+        'id': contact.id,
+        'name': contact.name,
+        'items': [{
+            'key': item.key,
+            'value': item.value,
+        } for item in items]
+    }
